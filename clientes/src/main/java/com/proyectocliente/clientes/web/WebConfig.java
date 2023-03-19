a2581d5143c028ad840d4ec843a79ba3d02e4949
Alexis Alberto Masin <AlexisMasin86@gmail.com>
2023-03-19 19:04:54 -0300

    Nuevos cambios
    Seguridad de SpringBoot

--- clientes/src/main/java/com/proyectocliente/clientes/web/WebConfig.java
+++ clientes/src/main/java/com/proyectocliente/clientes/web/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -29,5 +30,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registro){
         registro.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro){
+        registro.addViewController("/").setViewName("index");
+        registro.addViewController("/login");
     }
 }
