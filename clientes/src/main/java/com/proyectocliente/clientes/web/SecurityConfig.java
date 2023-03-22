906ebfc169127f387bf2e0304d8a7fa006f4a167
Alexis Alberto Masin <AlexisMasin86@gmail.com>
2023-03-21 21:52:48 -0300

    Seguridad con JPA
    Se implementó seguridad para inciar sesion con usuarios guardados desde una base de datos

--- clientes/src/main/java/com/proyectocliente/clientes/web/SecurityConfig.java
+++ clientes/src/main/java/com/proyectocliente/clientes/web/SecurityConfig.java
@@ -1,15 +1,34 @@
 package com.proyectocliente.clientes.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    /* Este bloque de codigo nos sirve para generar usuarios y password de manera local
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -21,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{noop}123")
                 .roles("USER");
     }
+    */
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
